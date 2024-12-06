f543675c1dc601c543868733d1878a633ef89b9d
Kayton Froeschl <kaytonfroeschl@Kaytons-MacBook-Pro-2.local>
2024-12-06 13:07:19 -0500

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Amplify } from "aws-amplify";
+import awsExports from "./aws-exports";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 Amplify.configure(awsExports);
 
@@ -11,16 +16,14 @@ const client = new ApolloClient({
   uri: "https://bttl34e7ejbtbg3fs5y4qcz364.appsync-api.us-east-1.amazonaws.com/graphql",
   cache: new InMemoryCache(),
   headers: {
-    'X-Api-Key': "da2-nyg3zamvgvbengscw6xtnaallu"
+    "X-Api-Key": "da2-3rot2pwcd5fdpibb5irwrtnsuy",
   },
-  connectToDevTools: true
+  connectToDevTools: true,
 });
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-    
 );
